3041707d3a226a0857963d08a082d42e3e43c9c0
Caio Gallo Barreira <caiogallobarreira@gmail.com>
2024-07-09 22:50:16 -0300

    commit inicial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<header class="p-6 bg-slate-300">
+  <h1 class="text-center text-3xl font-bold">Sharedoc</h1>
+	<p class="text-center text-gray-600">Experimento criado usando Sveltekit e Supabase</p>
+</header>
+
+<main class="p-8">
+	<slot></slot>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Textarea from '@/components/ui/textarea/textarea.svelte';
+	import Button from "@/components/ui/button/button.svelte";
+
+	let value = "";
+</script>
+
+<div id="toolbar" class="my-2 flex gap-2">
+	<Button>Bold</Button>
+	<Button>Italic</Button>
+</div>
+
+<Textarea bind:value></Textarea>
+
+{@html value}
